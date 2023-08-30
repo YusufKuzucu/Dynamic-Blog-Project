3a4e2b41625dc8766e644b06edc1db1bf5300142
yusuf <yusufkuzucu01@gmail.com>
2023-08-30 16:24:20 +0300

    Abone ol Bülteni yapıldı ve yorum yapma özelliği ekledik

---
+++ CoreDemo/Controllers/NewsLetterController.cs
@@ -0,0 +1,26 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis;
+
+namespace CoreDemo.Controllers
+{
+	public class NewsLetterController : Controller
+	{
+		NewsLetterManager nl = new NewsLetterManager(new EfNewsLetterRepository());
+		[HttpGet]
+		public PartialViewResult SubscribeMail()
+		{
+			return PartialView();
+		}
+		[HttpPost]
+		public PartialViewResult SubscribeMail(NewsLetter p)
+		{
+			p.MailStatus = true;
+			nl.AddNewsLetter(p);
+			
+			return PartialView();
+		}
+	}
+}
--- DataAccessLayer/Concrete/Context.cs
+++ DataAccessLayer/Concrete/Context.cs
@@ -20,6 +20,7 @@ namespace DataAccessLayer.Concrete
         public DbSet<Comment> Comments { get; set; }
         public DbSet<Contact> Contacts { get; set; }
         public DbSet<Writer> Writers { get; set; }
+		public DbSet<NewsLetter> NewsLetters { get; set; }
 
 
 
@@ -27,5 +28,9 @@ namespace DataAccessLayer.Concrete
 
 
 
-    }
+
+
+
+
+	}
 }
