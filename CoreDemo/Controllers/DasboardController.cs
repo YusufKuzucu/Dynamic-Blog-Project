480eeb230c9763da9e2e91ba4567104847d2ea05
yusuf <yusufkuzucu01@gmail.com>
2023-09-19 11:33:10 +0300

    Yazar Profil Sayfası tasarlandı

--- CoreDemo/Controllers/DasboardController.cs
+++ CoreDemo/Controllers/DasboardController.cs
@@ -1,13 +1,21 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace CoreDemo.Controllers
 {
     public class DasboardController : Controller
     {
+       
+        Context context = new Context();
+
         [AllowAnonymous]
         public IActionResult Index()
         {
+            ViewBag.v1 =context.Blogs.Count().ToString();
+            ViewBag.v2 =context.Blogs.Where(x=>x.WriterID==1).Count();
+            ViewBag.v3 = context.Categories.Count();
             return View();
         }
     }
--- CoreDemo/Controllers/WriterController.cs
+++ CoreDemo/Controllers/WriterController.cs
@@ -1,10 +1,20 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using CoreDemo.Models;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
 
 namespace CoreDemo.Controllers
 {
 	public class WriterController : Controller
 	{
+		WriterManager manager = new WriterManager(new EfWriterRepository());
 		public IActionResult Index()
 		{
 			return View();
@@ -24,5 +34,64 @@ namespace CoreDemo.Controllers
 		{
 			return PartialView();
 		}
-	}
+		[AllowAnonymous]
+		[HttpGet]
+		public IActionResult WriterEditProfile()
+		{
+			var writerValues = manager.GetById(1);
+			return View(writerValues);
+		}
+		[AllowAnonymous]	
+        [HttpPost]	
+        public IActionResult WriterEditProfile(Writer p)
+        {
+			WriterValidator wl=new WriterValidator();
+			var result=wl.Validate(p);
+			if (result.IsValid)
+			{
+                manager.TUpdate(p);
+
+				return RedirectToAction("Index", "Dasboard");
+
+            }
+			else
+			{
+                foreach (var item in result.Errors)
+                {
+					ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
+		[AllowAnonymous]
+		[HttpGet]
+		public IActionResult WriterAdd()
+		{
+			return View();
+		}
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult WriterAdd(AddProfileImage p)
+        {
+			Writer w = new Writer();
+			if (p.WriterImage != null)
+			{
+				var extension = Path.GetExtension(p.WriterImage.FileName);
+				var newImageName = Guid.NewGuid() + extension;
+				var location=Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/WriterImageFiles/",newImageName);
+				var stream = new FileStream(location, FileMode.Create);
+				p.WriterImage.CopyTo(stream);
+				w.WriterImage = newImageName;
+
+            }
+			w.WriterMail = p.WriterMail;
+			w.WriterPassword = p.WriterPassword;
+			w.WriterStatus = true;
+			w.WriterName = p.WriterName;
+			w.WriterAbout = p.WriterAbout;
+			manager.TAdd(w);
+            return RedirectToAction("Index", "Dasboard");
+
+        }
+    }
 }
