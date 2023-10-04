bf29f29e78b1e56cb70f5bb33d3ac8eff2b81c2d
yusuf <yusufkuzucu01@gmail.com>
2023-10-04 12:51:19 +0300

    İdentity kütüphanesi eklendi register konfigurasyonu yapıldı

---
+++ CoreDemo/Controllers/RegisterUserController.cs
@@ -0,0 +1,50 @@
+﻿using CoreDemo.Models;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace CoreDemo.Controllers
+{
+    [AllowAnonymous]
+    public class RegisterUserController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        public RegisterUserController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task< IActionResult> Index(UserSignUpViewModel userSign)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUser app = new AppUser()
+                {
+                    Email=userSign.Mail,
+                    UserName=userSign.UserName,
+                    NameSurname=userSign.NameSurname,
+                };
+                var result=await _userManager.CreateAsync(app,userSign.Password);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Login");
+                }
+                else
+                {
+                    foreach (var item in result.Errors)
+                    {
+                        ModelState.AddModelError("", item.Description);
+                    }
+                }
+            }
+            return View(userSign);
+        }
+    }
+}
