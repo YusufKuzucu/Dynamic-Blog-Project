e063dc2e6f00b128354f31866689b996a0b75448
yusuf <yusufkuzucu01@gmail.com>
2023-10-07 16:14:01 +0300

    Admin paneli revizeler başlandı mesjlar bölümü tasarlandı.

--- CoreDemo/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
+++ CoreDemo/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
@@ -16,10 +16,11 @@ namespace CoreDemo.Areas.Admin.ViewComponents.Statistic
             ViewBag.v1= bm.GetList().Count;
             ViewBag.v2= c.Contacts.Count();
             ViewBag.v3= c.Comments.Count();
-            string api = "api key eklenecek buraya";
-            string connection = "https://api.openweathermap.org/data/2.5/weather?q=istanbul&mode=xml&lang=tr&units=metric&appid="+api;
-            XDocument document = XDocument.Load(connection);
-            ViewBag.v4=document.Descendants("temperature").ElementAt(0).Attribute("value").Value;
+            //string api = "api key eklenecek buraya";
+            //string connection = "https://api.openweathermap.org/data/2.5/weather?q=istanbul&mode=xml&lang=tr&units=metric&appid="+api;
+            //XDocument document = XDocument.Load(connection);
+            //ViewBag.v4=document.Descendants("temperature").ElementAt(0).Attribute("value").Value;
+            ViewBag.v4 = 20;
 
             return View();
         }
