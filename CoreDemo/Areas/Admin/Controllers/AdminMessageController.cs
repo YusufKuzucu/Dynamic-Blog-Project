41aa89af05db601c547dc3659bd0f58001f06877
yusuf <yusufkuzucu01@gmail.com>
2023-10-08 13:17:19 +0300

    admin de gelen kutusu giden kutusu mesaj gönder özellikleri tamamlandı.

--- CoreDemo/Areas/Admin/Controllers/AdminMessageController.cs
+++ CoreDemo/Areas/Admin/Controllers/AdminMessageController.cs
@@ -1,11 +1,34 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using DocumentFormat.OpenXml.ExtendedProperties;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace CoreDemo.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class AdminMessageController : Controller
     {
+        Message2Manager manager = new Message2Manager(new EfMessage2Repository());
+        Context c = new Context();
         public IActionResult InBox()
+        {
+            var userName = User.Identity.Name;
+            var userMail = c.Users.Where(x => x.UserName == userName).Select(x => x.Email).FirstOrDefault();
+            var writerID = c.Writers.Where(x => x.WriterMail == userMail).Select(x => x.WriterID).FirstOrDefault();
+            var values = manager.GetInboxListByWriter(writerID);
+            return View(values);
+        }
+        public IActionResult SendBox()
+        {
+            var userName = User.Identity.Name;
+            var userMail = c.Users.Where(x => x.UserName == userName).Select(x => x.Email).FirstOrDefault();
+            var writerID = c.Writers.Where(x => x.WriterMail == userMail).Select(x => x.WriterID).FirstOrDefault();
+            var values = manager.GetSendboxListByWriter(writerID);
+            return View(values);
+        }
+        public IActionResult ComposeMessage()
         {
             return View();
         }
