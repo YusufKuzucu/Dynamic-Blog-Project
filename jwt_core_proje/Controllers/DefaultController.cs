7c52fb2b43c918e8d8f18ee65ad05c353c1c4d4d
yusuf <yusufkuzucu01@gmail.com>
2023-10-10 12:20:52 +0300

    Örnek Jwt token oluşturuldu ve test edildi.

---
+++ jwt_core_proje/Controllers/DefaultController.cs
@@ -0,0 +1,24 @@
+﻿using jwt_core_proje.Dal;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace jwt_core_proje.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+        [HttpGet("[action]")]
+        public IActionResult Login()
+        {
+            return Created("", new BuildToken().CreateToken());
+        }
+        [Authorize]
+        [HttpGet("[action]")]
+        public IActionResult PageOne()
+        {
+            return Ok("sayfa bir için giriş başarılı");
+        }
+    }
+}
