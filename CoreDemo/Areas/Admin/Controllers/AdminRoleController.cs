1530bc48e8bab1f2cccb24bcb37f3aac9ac9e4e9
yusuf <yusufkuzucu01@gmail.com>
2023-10-10 11:28:24 +0300

    Rol yapısı bitti gerekli erişim izinleri verildi

--- CoreDemo/Areas/Admin/Controllers/AdminRoleController.cs
+++ CoreDemo/Areas/Admin/Controllers/AdminRoleController.cs
@@ -1,19 +1,27 @@
-﻿using CoreDemo.Models;
+﻿using CoreDemo.Areas.Admin.Models;
+using CoreDemo.Models;
+using DocumentFormat.OpenXml.Office2010.ExcelAc;
 using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace CoreDemo.Areas.Admin.Controllers
 {
     [Area("Admin")]
+    [Authorize(Roles ="Admin")]
     public class AdminRoleController : Controller
     {
         private readonly RoleManager<AppRole> _roleManager;
-        public AdminRoleController(RoleManager<AppRole> roleManager)
+        private readonly UserManager<AppUser> _userManager;
+
+        public AdminRoleController(RoleManager<AppRole> roleManager, UserManager<AppUser> userManager)
         {
             _roleManager = roleManager;
+            _userManager = userManager;
         }
 
         public IActionResult Index()
@@ -81,6 +89,54 @@ namespace CoreDemo.Areas.Admin.Controllers
                     return RedirectToAction("Index");
                 }
             return View();
+        }
+        public  IActionResult UserRoleList()
+        {
+            var values =  _userManager.Users.ToList();
+            return View(values);
+        }
+        [HttpGet]
+        public async Task<IActionResult> AssignRole(int id)
+        {
+            var user=_userManager.Users.FirstOrDefault(x => x.Id == id);
+            var roles = _roleManager.Roles.ToList();
+
+            TempData["UserId"]=user.Id;
+
+
+            var userRoles = await _userManager.GetRolesAsync(user);
+
+            List<RoleAssignModel> model = new List<RoleAssignModel>();
+            foreach (var item in roles)
+            {
+                RoleAssignModel roleAssignModel = new RoleAssignModel();    
+                roleAssignModel.RoleID = item.Id;
+                roleAssignModel.Name= item.Name;
+                roleAssignModel.Exists = userRoles.Contains(item.Name);
+                model.Add(roleAssignModel);
+            }
+            return View(model);
+
+        }
+        [HttpPost]
+        public async Task<IActionResult> AssignRole(List<RoleAssignModel> model)
+        {
+            var userid = (int)TempData["UserId"];
+            var user=_userManager.Users.FirstOrDefault(x=>x.Id==userid);
+            foreach (var item in model )
+            {
+                if (item.Exists)
+                {
+                    await _userManager.AddToRoleAsync(user, item.Name);
+                }
+                else
+                {
+                    await _userManager.RemoveFromRoleAsync(user, item.Name);
+                }
+            }
+
+
+            return RedirectToAction("UserRoleList");
         }
     }
 }
---
+++ CoreDemo/Areas/Admin/Models/RoleAssignModel.cs
@@ -0,0 +1,9 @@
+﻿namespace CoreDemo.Areas.Admin.Models
+{
+    public class RoleAssignModel
+    {
+        public int RoleID { get; set; }
+        public string Name { get; set; }
+        public bool Exists { get; set; }
+    }
+}
